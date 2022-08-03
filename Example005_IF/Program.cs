16184a7fba0a90ad9e29c82f5831d39b0513164b
«SERGEY» <szely@yandex>
2022-08-03 16:11:59 +0300

    Привет Маша

---
+++ Example005_IF/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя польхователя: ");
+string username = Console .ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
